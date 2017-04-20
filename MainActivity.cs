ccab827c2af2ceb1606ddeb893867b0131c5b67f
yousefbn <yousef199400@hotmail.com>
2017-04-20 12:15:48 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System;
+using Android.Telephony.Gsm;
+
+namespace VerificationCodeTask
+{
+    [Activity(Label = "VerificationCodeTask", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button b;
+        EditText phone;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            b = FindViewById<Button>(Resource.Id.myButton);
+            phone = FindViewById<EditText>(Resource.Id.etphone);
+
+            b.Click += B_Click;
+         
+        }
+
+        private void B_Click(object sender, System.EventArgs e)
+        {
+            if (phone.Text == "")
+            {
+                Toast.MakeText(this, "Enter Your phone number", ToastLength.Long).Show();
+            }
+
+            else
+            {
+                var phoneNum = phone.Text;
+                string msg = GenerateRandomNo();
+
+                var piSent = PendingIntent.GetBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+                var piDelivered = PendingIntent.GetBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+                SmsManager.Default.SendTextMessage(phoneNum, null,
+#pragma warning restore CS0618 // Type or member is obsolete
+                msg, null, null);
+
+               
+
+
+              var intent = new Intent(this, typeof(Verifecation));
+                intent.PutExtra("Code", msg);
+                StartActivity(intent);
+
+
+
+
+            }
+            
+            
+            
+        
+    }
+        
+
+        public string GenerateRandomNo()
+        {
+            int _min = 00000;
+            int _max = 99999;
+            Random _rdm = new Random();
+            return _rdm.Next(_min, _max)+"";
+        }
+
+    }
+}
+
---
+++ Verifecation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace VerificationCodeTask
+{
+    [Activity(Label = "Verifecation")]
+    public class Verifecation : Activity
+    {
+        EditText et1, et2, et3, et4, et5;
+        Button btCheck;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.VerificationActivity);
+            et1 = FindViewById<EditText>(Resource.Id.editText1);
+            et2 = FindViewById<EditText>(Resource.Id.editText2);
+            et3 = FindViewById<EditText>(Resource.Id.editText3);
+            et4 = FindViewById<EditText>(Resource.Id.editText4);
+            et5 = FindViewById<EditText>(Resource.Id.editText5);
+            btCheck = FindViewById<Button>(Resource.Id.button1);
+
+            btCheck.Click += BtCheck_Click;
+        }
+
+        private void BtCheck_Click(object sender, EventArgs e)
+        {
+            StringBuilder b = new StringBuilder();
+            b.Append(et1.Text);
+            b.Append(et2.Text);
+            b.Append(et3.Text);
+            b.Append(et4.Text);
+            b.Append(et5.Text);
+
+            string verCode = Intent.GetStringExtra("Code") ?? "Data not available";
+
+            if (b.ToString() == verCode)
+            {
+
+                Toast.MakeText(this, "Valid Code", ToastLength.Long).Show();
+                
+            }
+            else
+            {
+
+                AlertDialog.Builder inValid = new AlertDialog.Builder(this);
+                inValid.SetTitle("inValid");
+                inValid.SetMessage("Your Code is inCorrect");
+                inValid.SetCancelable(true);
+                inValid.SetNegativeButton("Ok", delegate { SetContentView(Resource.Layout.VerificationActivity); });
+                inValid.Show();
+
+            }
+                
+
+
+
+           /* if (b.ToString() == verCode)
+                Toast.MakeText(this, "Code is Correct", ToastLength.Long).Show();
+            else
+                Toast.MakeText(this, "Code is incoreect", ToastLength.Long).Show();*/
+
+        }
+    }
+}
